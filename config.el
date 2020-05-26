3a05c3c59ddbf504e787afafd67c200e1fc3b4fb
Toby Law <toby@tzcl.me>
2020-05-26 16:45:11 +1000

    Set up company box

--- config.el
+++ config.el
@@ -37,11 +37,14 @@
 ;; Make focus mode work with paragraphs
   (setq focus-mode-to-thing '((prog-mode . defun) (text-mode . paragraph))))
 
-;; Define paragraphs in text-mode to include lists (and make sure auto-fill is enabled)
+;; Define paragraphs in text-mode to include lists
 (defun toby/text-mode-hook ()
   (setq paragraph-start "\f\\|[ \t]*$")
   (setq paragraph-separate "^[ \t\f]*$"))
 (add-hook 'text-mode-hook #'toby/text-mode-hook)
+
+;; Make sure auto-fill-mode is enabled
+(setq-default auto-fill-function 'do-auto-fill)
 
 ;;
 ;;; UI
