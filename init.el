94f905f0c30fb7afc0b98a48dc60d48c951e2b50
Toby Law <toby@tzcl.me>
2021-06-29 13:08:51 +1000

    Disabled grammar module

--- init.el
+++ init.el
@@ -82,7 +82,7 @@
        (syntax
         +childframe)     ; tasing you for every semicolon you forget
        spell             ; tasing you for misspelling mispelling
-       grammar           ; tasing grammar mistake every you make
+       ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
