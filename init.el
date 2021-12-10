2cc4fcc94d3082fff1197e6f9313dc9207090ec5
Toby Law <toby@tzcl.me>
2021-12-10 12:39:11 +1100

    Installed guile (for SICP)

--- init.el
+++ init.el
@@ -165,7 +165,7 @@
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       ;;scheme            ; a fully conniving family of lisps
+       (scheme +guile)            ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
