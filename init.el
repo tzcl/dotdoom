43956c54d613ebeb4330985ed550cc6fc50bcec7
Toby Law <toby@tzcl.me>
2022-03-12 21:24:35 +1100

    Removed plantuml

--- init.el
+++ init.el
@@ -155,7 +155,7 @@
         +present)        ; using org-mode for presentations
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
-       plantuml          ; diagrams for confusing people more
+       ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python
         +lsp)            ; beautiful is better than ugly
